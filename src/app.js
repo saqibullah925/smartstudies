0ff1c914f8b1a057d9bf1f3ae5ee6917ce1868b2
saqib ullah <saqibullah925@gmail.com>
2026-09-14 13:59:10 +0500

    Add health check endpoint

--- src/app.js
+++ src/app.js
@@ -2,4 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/health",(req, res) =>{
+    res.send({
+  "status": "ok"
+})
+}) 
+
 module.exports = app
--- src/server.js
+++ src/server.js
@@ -4,6 +4,13 @@ const config = require("./config/env");
 
 const port = config.port;
 
+app.get("/health",(req, res) =>{
+    res.send({
+  "status": "ok"
+})
+}) 
+
+
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
 
