91ed5d7d7cb55b419038936167052fa6a3978efd
saqib ullah <saqibullah925@gmail.com>
2026-09-22 12:47:14 +0500

    Prepare authentication dependencies and configuration

--- src/config/env.js
+++ src/config/env.js
@@ -9,6 +9,7 @@ require("dotenv").config();
 
 const port = Number(process.env.PORT) || 5000;
 const mongoUri = process.env.MONGO_URI;
+const jwtSecret = process.env.JWT_SECRET;
 
 module.exports = {
     port,
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -6,14 +6,14 @@ const userSchema = new mongoose.Schema({
         required: true
     },
     email:{
-        type : String,
         required: true,
-        unique: true
+        unique: true,
+        lowercase: true
     },
     password:{
-        type:String,
         required:true
-    } 
+    },
+    timestamps: true 
     
 
 });
